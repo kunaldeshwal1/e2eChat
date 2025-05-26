e7931501fc576646aa9f85804d2af5b1dd181e1a
kunal <deshwal170@gmail.com>
2025-05-26 11:35:44 +0530

    renamed messages.ts to message.ts

--- backend/src/routes/messages.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { prismaClient } from "../prismaClient";
-const router = express.Router();
-router.get("/all", async (req, res) => {
-  res.json({ mesage: "Hello" });
-});
-
-export const messageRouter = router;
