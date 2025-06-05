7f9d8a75a0869e3369cc399ce0d9a7eb7024a7d2
kunal <deshwal170@gmail.com>
2025-06-05 10:39:21 +0530

    added cookie in allowedHeaders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(
         : "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   })
 );
 const io = new Server(httpServer, {
