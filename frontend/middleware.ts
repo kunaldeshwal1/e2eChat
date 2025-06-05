d93bf72acf225f2b8d58c7471c96a33ce1af7078
kunal <deshwal170@gmail.com>
2025-06-05 11:00:18 +0530

    added log in client middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -16,7 +16,7 @@ export default async function middleware(req: NextRequest) {
 
   const cookieStore = await cookies();
   const session = cookieStore.get("session");
-
+  console.log("this is seesion from client middleware", session);
   if (isProtectedRoute && !session) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
