09b35de8fe3feb56e74b8b5b66c776229dbf4d9d
kunal <deshwal170@gmail.com>
2025-06-27 23:49:13 +0530

    fixed navbar open issue

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -12,7 +12,7 @@ dotenv.config();
 const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 export default function Navbar() {
   const { currentUserName, setCurrentUserName } = useUser();
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const pathname = usePathname();
   const router = useRouter();
 
@@ -37,53 +37,46 @@ export default function Navbar() {
           onClick={() => setOpen(!open)}
         >
           <span
-            className={`block w-7 h-1 bg-white mb-1.5 rounded transition-all ${
-              open ? "rotate-45 translate-y-2" : ""
-            }`}
+            className={`block w-7 h-1 bg-white mb-1.5 rounded transition-all ${open ? "rotate-45 translate-y-2" : ""
+              }`}
           ></span>
           <span
-            className={`block w-7 h-1 bg-white mb-1.5 rounded transition-all ${
-              open ? "opacity-0" : ""
-            }`}
+            className={`block w-7 h-1 bg-white mb-1.5 rounded transition-all ${open ? "opacity-0" : ""
+              }`}
           ></span>
           <span
-            className={`block w-7 h-1 bg-white rounded transition-all ${
-              open ? "-rotate-45 -translate-y-3" : ""
-            }`}
+            className={`block w-7 h-1 bg-white rounded transition-all ${open ? "-rotate-45 -translate-y-3" : ""
+              }`}
           ></span>
         </button>
         <ul
-          className={`md:static md:flex md:gap-12 ${
-            open
-              ? "flex flex-col absolute right-0 top-24 rounded-b-sm  bg-blue-900"
-              : "hidden"
-          } transition-all`}
+          className={`md:static md:flex md:gap-12 ${open
+            ? "flex flex-col absolute right-0 top-24 rounded-b-sm  bg-blue-900"
+            : "hidden"
+            } transition-all`}
           onClick={() => setOpen(false)}
         >
           {currentUserName ? (
             <li className="flex gap-8 items-center">
               <Link
                 href="/dashboard"
-                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${
-                  pathname === "/dashboard" ? "text-gray-400" : ""
-                }`}
+                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${pathname === "/dashboard" ? "text-gray-400" : ""
+                  }`}
               >
                 Groups
               </Link>
 
               <Link
                 href="/users"
-                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${
-                  pathname === "/users" ? "text-gray-400" : ""
-                }`}
+                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${pathname === "/users" ? "text-gray-400" : ""
+                  }`}
               >
                 Users
               </Link>
               <Link
                 href="/mycontacts"
-                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${
-                  pathname === "/mycontacts" ? "text-gray-400" : ""
-                }`}
+                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${pathname === "/mycontacts" ? "text-gray-400" : ""
+                  }`}
               >
                 My Contacts
               </Link>
@@ -104,27 +97,24 @@ export default function Navbar() {
             <li className="flex flex-col p-4 md:static md:flex-row gap-8">
               <Link
                 href="/"
-                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${
-                  pathname === "/" ? "text-gray-500" : ""
-                }`}
+                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${pathname === "/" ? "text-gray-500" : ""
+                  }`}
               >
                 Home
               </Link>
 
               <Link
                 href="/login"
-                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${
-                  pathname === "/login" ? "text-gray-500" : ""
-                }`}
+                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${pathname === "/login" ? "text-gray-500" : ""
+                  }`}
               >
                 Login
               </Link>
 
               <Link
                 href="/register"
-                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${
-                  pathname === "/register" ? "text-gray-500" : ""
-                }`}
+                className={`transition delay-150 duration-300 ease-in-out hover:scale-120 ${pathname === "/register" ? "text-gray-500" : ""
+                  }`}
               >
                 Sign up
               </Link>
